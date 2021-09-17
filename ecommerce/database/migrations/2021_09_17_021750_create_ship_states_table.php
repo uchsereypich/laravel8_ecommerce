828b9c65742fd3119378f7060b4771dac015c811
uchsereypich <uchsereypich@gmail.com>
2021-09-17 14:51:06 +0700

    ecommerce project

---
+++ ecommerce/database/migrations/2021_09_17_021750_create_ship_states_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShipStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ship_states', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ship_states');
+    }
+}
